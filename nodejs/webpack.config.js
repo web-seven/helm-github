ad5a877932eb70ecca31429227731053fc122dcf
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2020-09-04 15:24:56 +0300

    Build release action

--- nodejs/webpack.config.js
+++ nodejs/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,7 +9,17 @@ module.exports = {
   mode: 'production',
   target: "node",
   output: {
-    filename: '[name].js',
+    filename: 'src/[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'bin', to: 'bin' },
+        { from: 'plugin.yaml', to: 'plugin.yaml' },
+        { from: '../LICENSE', to: '' },
+        { from: '../README.md', to: '' },
+      ],
+    }),
+  ],
 };
