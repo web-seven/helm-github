5001a8e278ef89d4c221276fd885389f5900670d
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2021-12-13 18:43:09 +0200

    fixed pagination limited releases from github

--- nodejs/src/release.js
+++ nodejs/src/release.js
@@ -15,50 +15,69 @@ const octokit = new Octokit({
 const releaseUrl = process.argv[2];
 let [owner, repo, releaseName] = releaseUrl.replace('github+release://', '').replace('.tgz', '').split('/');
 
-octokit.repos.
-    listReleases({
-        owner: owner,
-        repo: repo,
-    })
-    .then(({ data }) => {
-        data.forEach((releaseData) => {
-            if (releaseData.name == releaseName) {
-                let assetUrl = releaseData.assets[0].url;
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        Accept: 'application/octet-stream',
-                        Authorization: 'token ' + GITHUB_TOKEN,
-                        "User-Agent": 'Helm-Plugin-Github'
-                    }
-                };
 
-                const req = https.request(assetUrl, options, (res) => {
+async function getRelease() {
 
-                    if (res.statusCode == 302) {
-                        const redReq = https.request(res.headers.location, (res) => {
+    let pageNumber = 0;
+    let loadNextPage = true;
 
-                            res.on('data', (d) => {
-                                process.stdout.write(d);
+    while (loadNextPage) {
+        pageNumber++;
+        await octokit.repos.
+            listReleases({
+                owner: owner,
+                repo: repo,
+                page: pageNumber,
+                per_page: 100
+            })
+            .then(({ data }) => {
+                if (data.length == 0) {
+                    loadNextPage = false;
+                } else {
+                    data.forEach((releaseData) => {
+                        if (releaseData.name == releaseName) {
+                            let assetUrl = releaseData.assets[0].url;
+                            const options = {
+                                method: 'GET',
+                                headers: {
+                                    Accept: 'application/octet-stream',
+                                    Authorization: 'token ' + GITHUB_TOKEN,
+                                    "User-Agent": 'Helm-Plugin-Github'
+                                }
+                            };
+
+                            const req = https.request(assetUrl, options, (res) => {
+
+                                if (res.statusCode == 302) {
+                                    const redReq = https.request(res.headers.location, (res) => {
+
+                                        res.on('data', (d) => {
+                                            process.stdout.write(d);
+                                        });
+                                    });
+
+                                    redReq.on('error', (e) => {
+                                        process.stderr.write(e);
+                                    });
+                                    redReq.end();
+                                } else {
+                                    res.on('data', (d) => {
+                                        process.stdout.write(d);
+                                    });
+                                }
                             });
-                        });
 
-                        redReq.on('error', (e) => {
-                            process.stderr.write(e);
-                        });
-                        redReq.end();
-                    } else {
-                        res.on('data', (d) => {
-                            process.stdout.write(d);
-                        });
-                    }
-                });
+                            req.on('error', (e) => {
+                                process.stderr.write(e);
+                            });
+                            req.end();
+                            loadNextPage = false;
+                        }
+                    })
+                }
+            });
+    }
+}
 
-                req.on('error', (e) => {
-                    process.stderr.write(e);
-                });
-                req.end();
-            }
-        })
-    });
+getRelease();
 
--- nodejs/src/repo.js
+++ nodejs/src/repo.js
@@ -18,57 +18,75 @@ const octokit = new Octokit({
 const releaseUrl = process.argv[2];
 let [owner, repo] = releaseUrl.replace('github://', '').split('/');
 
-octokit.repos.
-    listReleases({
-        owner: owner,
-        repo: repo,
-    })
-    .then(({ data }) => {
+const repoData = {
+    apiVersion: 'v1',
+    entries: {
+    }
+}
 
-        let repoData = {
-            apiVersion: 'v1',
-            entries: {
-            }
-        }
+async function getReleases() {
 
-        data.forEach((release) => {
-            if(!release['name']) {
-                return;
-            }
-            const releaseParts = release.name.split("-");
-            const releaseVersion = [releaseParts[releaseParts.length-2], releaseParts[releaseParts.length-1]].join('-');
-            const version = release.name.substr(release.name.indexOf(semver.coerce(releaseVersion)));
-            const chartName = release.name.replace('-' + version, '');
+    let pageNumber = 0;
+    let loadNextPage = true;
 
-            if (repoData.entries[chartName] == undefined) {
-                repoData.entries[chartName] = [];
-            }
-            if(release.assets) {
-                release.assets.forEach((asset)=>{
-                    const releaseData = {
-                        apiVersion: "v2",
-                        version: version,
-                        name: chartName,
-                        appVersion: asset.label,
-                        type: 'application',
-                        description: release.body,
-                        digest: release.node_id,
-                        created: release.created_at,
-                        urls: [
-                            'github+release://' + owner + '/' + repo + '/' + release.name + '.tgz'
-                        ]
-                    };
-                    repoData.entries[chartName].push(releaseData);
-                })
-            }
-        })
 
-        
-        if(HELM_GITHUB_MERGE_INDEX_FILE && fs.existsSync(HELM_GITHUB_MERGE_INDEX_FILE)) {
-            const mergeRepoIndex = yaml.load(fs.readFileSync(HELM_GITHUB_MERGE_INDEX_FILE));
-            repoData = merge(mergeRepoIndex, repoData);
-        }
-        
-        process.stdout.write(yaml.safeDump(repoData));
-    });
+    if (HELM_GITHUB_MERGE_INDEX_FILE && fs.existsSync(HELM_GITHUB_MERGE_INDEX_FILE)) {
+        const mergeRepoIndex = yaml.load(fs.readFileSync(HELM_GITHUB_MERGE_INDEX_FILE));
+        repoData = merge(mergeRepoIndex, repoData);
+    }
+
+    while (loadNextPage) {
+        pageNumber++;
+        await octokit.repos.
+            listReleases({
+                owner: owner,
+                repo: repo,
+                page: pageNumber,
+                per_page: 100
+            })
+            .then(({ data }) => {
+                if (data.length == 0) {
+                    loadNextPage = false;
+                } else {
+                    data.forEach((release) => {
+
+                        if (release.name && release.assets && release.assets.length > 0) {
+                            const releaseParts = release.name.split("-");
+                            const releaseVersion = [releaseParts[releaseParts.length - 2], releaseParts[releaseParts.length - 1]].join('-');
+                            const version = release.name.substr(release.name.indexOf(semver.coerce(releaseVersion)));
+                            const chartName = release.name.replace('-' + version, '');
+
+                            if (repoData.entries[chartName] == undefined) {
+                                repoData.entries[chartName] = [];
+                            }
+                            release.assets.forEach((asset) => {
+                                const releaseData = {
+                                    apiVersion: "v2",
+                                    version: version,
+                                    name: chartName,
+                                    appVersion: asset.label,
+                                    type: 'application',
+                                    description: release.body,
+                                    digest: release.node_id,
+                                    created: release.created_at,
+                                    urls: [
+                                        'github+release://' + owner + '/' + repo + '/' + release.name + '.tgz'
+                                    ]
+                                };
+                                repoData.entries[chartName].push(releaseData);
+                            });
+                        }
+
+                    })
+
+                    process.stdout.write(yaml.safeDump(repoData));
+                }
+
+            });
+    }
+}
+
+getReleases();
+
+
 
