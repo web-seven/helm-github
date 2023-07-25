893277d31b329b6d70afd1a0e1edf5ad0d48820a
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2023-07-25 15:22:14 +0300

    fixed fetch-all delay

--- nodejs/src/modules/fetch-all.js
+++ nodejs/src/modules/fetch-all.js
@@ -2,7 +2,7 @@ const { Octokit } = require("@octokit/rest");
 const https = require('https');
 const fs = require('fs');
 const GITHUB_DELAY = process.env['GITHUB_DELAY'];
-function delay(time) {
+async function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 } 
 module.exports = async function (repoUrl, GITHUB_TOKEN) {
@@ -15,66 +15,65 @@ module.exports = async function (repoUrl, GITHUB_TOKEN) {
     let loadNextPage = true;
     console.log("Fetching all releases from " + owner + "/" + repo);
     while (loadNextPage) {
-        console.log("Fetching page " + pageNumber);
+        console.log(new Date().toString() + "Fetching page " + pageNumber);
         pageNumber++;
-        if(GITHUB_DELAY) {
-            await delay(parseInt(GITHUB_DELAY));
-        }
-        await octokit.repos.
-            listReleases({
-                owner: owner,
-                repo: repo,
-                page: pageNumber,
-                per_page: 100,
-            })
-            .then(({ data }) => {
-                if (data.length == 0) {
-                    loadNextPage = false;
+        let releases = [];
+        await octokit.repos.listReleases({
+            owner: owner,
+            repo: repo,
+            page: pageNumber,
+            per_page: 100,
+        })
+        .then(({ data }) => {
+            if (data.length == 0) {
+                loadNextPage = false;
+            } else {
+                releases = [...releases, ...data];
+            }
+        });
+
+        for (let releaseData of releases) {
+            let assetUrl = releaseData.assets[0].url;
+            let assetFileName = releaseData.assets[0].name;
+            const options = {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/octet-stream',
+                    Authorization: 'token ' + GITHUB_TOKEN,
+                    "X-GitHub-Api-Version": "2022-11-28",
+                    "User-Agent": 'Helm-Plugin-Github'
+                }
+            };
+            if(GITHUB_DELAY) {
+                await delay(parseInt(GITHUB_DELAY));
+            }
+            console.log(new Date().toString() + " Downloading " + assetFileName);
+            const req = https.request(assetUrl, options, (res) => {
+
+                if (res.statusCode == 302) {
+                    const redReq = https.request(res.headers.location, (res) => {
+
+                        res.on('data', (d) => {
+                            fs.appendFileSync(assetFileName, d);
+                        });
+                    });
+
+                    redReq.on('error', (e) => {
+                        process.stderr.write(e);
+                    });
+                    redReq.end();
                 } else {
-                    data.forEach(async (releaseData) => {
-                        let assetUrl = releaseData.assets[0].url;
-                        let assetFileName = releaseData.assets[0].name;
-                        console.log("Downloading " + assetFileName);
-                        const options = {
-                            method: 'GET',
-                            headers: {
-                                Accept: 'application/octet-stream',
-                                Authorization: 'token ' + GITHUB_TOKEN,
-                                "X-GitHub-Api-Version": "2022-11-28",
-                                "User-Agent": 'Helm-Plugin-Github'
-                            }
-                        };
-                        if(GITHUB_DELAY) {
-                            await delay(parseInt(GITHUB_DELAY));
-                        }
-                        const req = https.request(assetUrl, options, (res) => {
-
-                            if (res.statusCode == 302) {
-                                const redReq = https.request(res.headers.location, (res) => {
-
-                                    res.on('data', (d) => {
-                                        fs.appendFileSync(assetFileName, d);
-                                    });
-                                });
-
-                                redReq.on('error', (e) => {
-                                    process.stderr.write(e);
-                                });
-                                redReq.end();
-                            } else {
-                                res.on('data', (d) => {
-                                    fs.appendFileSync(assetFileName, d);
-                                });
-                            }
-                        });
-
-                        req.on('error', (e) => {
-                            process.stderr.write(e);
-                        });
-                        req.end();
-
-                    })
+                    res.on('data', (d) => {
+                        fs.appendFileSync(assetFileName, d);
+                    });
                 }
             });
+
+            req.on('error', (e) => {
+                process.stderr.write(e);
+            });
+            req.end();
+
+        };
     }
 }
